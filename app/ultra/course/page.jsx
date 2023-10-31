19f6b9126c415a9a17b82c3de8841b86dc58f2b0
qqwn <99554755+qqwn@users.noreply.github.com>
2023-10-31 18:56:05 +0900

    connect dbs

--- app/ultra/course/page.jsx
+++ app/ultra/course/page.jsx
@@ -1,7 +1,21 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>코스</h1>
-     </div>
-    )
-  }
+
+import React from "react";
+const Course = async () => { // 비동기 함수로 변경시켜줌
+  const response = await fetch("http://localhost:9999/courses", {
+    cache: "no-cache"
+  });
+  const courses = await response.json();
+  return (
+    <div>
+      {courses.map((course) => (
+        <div>
+          <span>
+            {course.code}_ {course.name}({course.courseId})
+          </span>
+          <span>{course.professtorName}</span>
+        </div>
+      ))}
+    </div>
+  );  
+};
+export default Course;
